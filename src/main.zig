541ffb623361d359757a1089be58beac038b0245
Pete Kazmier <pete@kazmier.com>
2025-08-10 14:19:47 -0400

    feat: fetches a URL and prints JSON response

--- src/main.zig
+++ src/main.zig
@@ -5,53 +5,35 @@ var buffer: [128]u8 = undefined;
 var writer = std.fs.File.stdout().writer(&buffer);
 const stdout = &writer.interface;
 
-const url = "http://httpbin.org/anything";
-
-const HttpBinResponse = struct {
-    method: []const u8,
-    origin: []const u8,
-    url: []const u8,
-    headers: std.json.Value,
-};
-
 pub fn main() !void {
     var gpa = std.heap.DebugAllocator(.{}).init;
     defer _ = gpa.detectLeaks();
     const allocator = gpa.allocator();
 
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    if (args.len != 2) {
+        std.debug.print("usage: {s} <url>\n", .{args[0]});
+        std.process.exit(1);
+    }
+    const url = args[1];
+
     var client = std.http.Client{ .allocator = allocator };
     defer client.deinit();
 
-    var timer = try std.time.Timer.start();
     const parsed = jsonfetch.fetch(
         &client,
-        *HttpBinResponse,
+        *std.json.Value,
         .{ .location = .{ .url = url } },
-        .{ .ignore_unknown_fields = true },
+        .{},
     ) catch |err| {
         std.debug.print("JSON fetch failed with {t}\n", .{err});
         std.process.exit(1);
     };
-    std.debug.print("Fetch took {}ms\n", .{timer.read() / std.time.ns_per_ms});
     defer parsed.deinit();
-    const value = parsed.value;
 
-    try stdout.print(
-        \\
-        \\url     = {s}
-        \\method  = {s}
-        \\origin  = {s}
-        \\headers =
-        \\
-    , .{ value.url, value.method, value.origin });
-
-    var it = value.headers.object.iterator();
-    while (it.next()) |hdr| {
-        try stdout.print("  {s}: {s}\n", .{
-            hdr.key_ptr.*,
-            hdr.value_ptr.*.string,
-        });
-    }
-
+    try std.json.Stringify.value(parsed.value, .{ .whitespace = .indent_3 }, stdout);
+    try stdout.print("\n", .{});
     try stdout.flush(); // Don't forget to flush!
 }
